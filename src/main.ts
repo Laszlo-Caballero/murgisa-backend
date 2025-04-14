670bb5cece92ad1e0d835b6b7696187b1fe34832
Laszlo-Caballero <laszlocaballero@gmail.com>
2025-04-13 20:56:43 -0500

    Agregar class-transformer y class-validator como dependencias; configurar ValidationPipe en el bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
+
+  app.setGlobalPrefix('api');
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
