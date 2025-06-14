e671677e0b493b7e84f40db9efd4ac26955ca0c2
laszlo-caballero <laszlocaballero@gmail.com>
2025-06-13 20:54:57 -0500

    feat(departamento): agregar DTO y entidad para la creación de departamentos

--- src/departamento/dto/create-departamento.dto.ts
+++ src/departamento/dto/create-departamento.dto.ts
@@ -1,1 +1,24 @@
-export class CreateDepartamentoDto {}
+import {
+  IsBoolean,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+  Min,
+} from 'class-validator';
+
+export class CreateDepartamentoDto {
+  @IsString()
+  @IsNotEmpty()
+  titulo: string;
+  @IsString()
+  @IsNotEmpty()
+  descripcion: string;
+  @IsNumber()
+  @Min(0)
+  presupuesto: number;
+
+  @IsBoolean()
+  @IsOptional()
+  estado: boolean;
+}
--- src/departamento/entities/departamento.entity.ts
+++ src/departamento/entities/departamento.entity.ts
@@ -2,10 +2,14 @@ import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class Departamento {
-    @PrimaryGeneratedColumn()
-    idDepartamento: number;
-    @Column({ length: 100 })
-    descripcion: string;
-    @Column({ default: true })
-    estado: boolean;
+  @PrimaryGeneratedColumn()
+  idDepartamento: number;
+  @Column({ length: 50, default: '' })
+  titulo: string;
+  @Column({ length: 100 })
+  descripcion: string;
+  @Column({ default: true })
+  estado: boolean;
+  @Column({ type: 'float', default: 0 })
+  presupuesto: number;
 }
