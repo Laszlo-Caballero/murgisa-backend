6eb3e990c46e95448f78bbe62e60895643fc1c9d
laszlo-caballero <laszlocaballero@gmail.com>
2025-06-20 21:14:23 -0500

    feat(departamento): agregar protección de rutas con JwtAuthGuard en el controlador de departamento

--- src/departamento/departamento.controller.ts
+++ src/departamento/departamento.controller.ts
@@ -1,32 +1,45 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+} from '@nestjs/common';
 import { DepartamentoService } from './departamento.service';
 import { CreateDepartamentoDto } from './dto/create-departamento.dto';
 import { UpdateDepartamentoDto } from './dto/update-departamento.dto';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
 @Controller('departamento')
 export class DepartamentoController {
   constructor(private readonly departamentoService: DepartamentoService) {}
-
+  @UseGuards(JwtAuthGuard)
   @Post()
   create(@Body() createDepartamentoDto: CreateDepartamentoDto) {
     return this.departamentoService.create(createDepartamentoDto);
   }
-
   @Get()
   findAll() {
     return this.departamentoService.findAll();
   }
-
+  @UseGuards(JwtAuthGuard)
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.departamentoService.findOne(+id);
   }
-
+  @UseGuards(JwtAuthGuard)
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateDepartamentoDto: UpdateDepartamentoDto) {
+  update(
+    @Param('id') id: string,
+    @Body() updateDepartamentoDto: UpdateDepartamentoDto,
+  ) {
     return this.departamentoService.update(+id, updateDepartamentoDto);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.departamentoService.remove(+id);
