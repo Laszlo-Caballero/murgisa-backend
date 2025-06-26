9fd3634e570b4a53f8c77c2da900766c237e83d5
laszlo-caballero <laszlocaballero@gmail.com>
2025-06-25 23:34:44 -0500

    feat(profesion): agregar entidad Log al módulo de Profesion

--- src/profesion/profesion.module.ts
+++ src/profesion/profesion.module.ts
@@ -3,9 +3,10 @@ import { ProfesionService } from './profesion.service';
 import { ProfesionController } from './profesion.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Profesion } from './entities/profesion.entity';
+import { Log } from 'src/home/entities/log.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Profesion])],
+  imports: [TypeOrmModule.forFeature([Profesion, Log])],
   controllers: [ProfesionController],
   providers: [ProfesionService],
 })
