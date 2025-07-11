b7e4327cf1d6c6bceb37589e0fde60a529a28fa1
luciana-31 <lgogny74@gmail.com>
2025-07-10 23:55:15 -0500

    feat(departamento): add logging functionality and improve response structure in service methods

--- src/departamento/departamento.service.ts
+++ src/departamento/departamento.service.ts
@@ -4,19 +4,36 @@ import { UpdateDepartamentoDto } from './dto/update-departamento.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Departamento } from './entities/departamento.entity';
 import { Repository } from 'typeorm';
+import { Log } from 'src/home/entities/log.entity';
 
 @Injectable()
 export class DepartamentoService {
     constructor(
       @InjectRepository(Departamento)
       private departamentoRespository: Repository<Departamento>,
+      @InjectRepository(Log)
+      private logRepository: Repository<Log>,
     ) {}
 
-  create(createDepartamentoDto: CreateDepartamentoDto) {
+  async create(createDepartamentoDto: CreateDepartamentoDto) {
     const newDepartamento = this.departamentoRespository.create(createDepartamentoDto);
 
-    return this.departamentoRespository.save(newDepartamento);
+    await this.departamentoRespository.save(newDepartamento);
+
+    await this.logRepository.save({
+      tipo: 'Departamento',
+      mensaje: `Nuevo Departamento ${newDepartamento.titulo} creado con exito`,
+    });
+
+    const departamentos = await this.departamentoRespository.find();
+
+    return {
+      message: 'Profesion created successfully',
+      status: 201,
+      data: departamentos,
+    };
   }
+
 
   async findAll() {
     const departamentos = await this.departamentoRespository.find();
@@ -29,10 +46,21 @@ export class DepartamentoService {
     });
 
     if (!departamento) {
-      throw new HttpException(`Departamento with id ${id} not found`, 404);
+      throw new HttpException(
+        {
+          message: `Departamento with id ${id} not found`,
+          status: 404,
+          data: null,
+        },
+        404,
+      );
     }
 
-    return departamento;
+        return {
+      message: 'Profesion retrieved successfully',
+      status: 200,
+      data: departamento,
+    };
   }
 
   async update(id: number, updateDepartamentoDto: UpdateDepartamentoDto) {
@@ -40,7 +68,14 @@ export class DepartamentoService {
       idDepartamento: id,
     });
     if (!departamento) {
-      throw new HttpException(`Departamento with id ${id} not found`, 404);
+      throw new HttpException(
+        {
+          message: `Departamento with id ${id} not found`,
+          status: 404,
+          data: null,
+        },
+        404,
+      );
     }
 
     await this.departamentoRespository.update(
@@ -52,9 +87,17 @@ export class DepartamentoService {
       },
     );
 
+    await this.logRepository.save({
+      tipo: 'Departamento',
+      mensaje: `Departamento ${departamento.titulo} actualizada con exito`,
+    });
+
+    const departamentos = await this.departamentoRespository.find();
+
     return {
       message: `Departamento with id ${id} has been updated successfully`,
-      status: true,
+      status: 201,
+      data: departamentos,
     };
   }
 
@@ -64,7 +107,14 @@ export class DepartamentoService {
     });
 
     if (!departamento) {
-      throw new HttpException(`Departamento with id ${id} not found`, 404);
+      throw new HttpException(
+        {
+          message: `Departamento with id ${id} not found`,
+          status: 404,
+          data: null,
+        },
+        404,
+      );
     }
 
     await this.departamentoRespository.update(
@@ -76,9 +126,17 @@ export class DepartamentoService {
       },
     );
 
+    await this.logRepository.save({
+      tipo: 'Departamento',
+      mensaje: `Departamento ${departamento.titulo} desactivada con exito`,
+    });
+
+    const departamentos = await this.departamentoRespository.find();
+
     return {
       message: `Departamento with id ${id} has been deactivated successfully`,
-      status: true,
+      status: 200,
+      data: departamentos,
     };
   }
 }
