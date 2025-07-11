a6d17dfc552c0b3e1a80dc0c738d81715f684d9e
laszlo-caballero <laszlocaballero@gmail.com>
2025-07-10 23:04:12 -0500

    feat(tipo-recurso): implementar DTO y métodos en el servicio para gestión de tipos de recurso

--- src/tipo-recurso/dto/create-tipo-recurso.dto.ts
+++ src/tipo-recurso/dto/create-tipo-recurso.dto.ts
@@ -1,1 +1,11 @@
-export class CreateTipoRecursoDto {}
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateTipoRecursoDto {
+  @IsString()
+  @IsNotEmpty()
+  nombre: string;
+
+  @IsString()
+  @IsNotEmpty()
+  descripcion: string;
+}
--- src/tipo-recurso/tipo-recurso.service.ts
+++ src/tipo-recurso/tipo-recurso.service.ts
@@ -1,26 +1,124 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
 import { CreateTipoRecursoDto } from './dto/create-tipo-recurso.dto';
 import { UpdateTipoRecursoDto } from './dto/update-tipo-recurso.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { TipoRecurso } from './entities/tipo-recurso.entity';
+import { Repository } from 'typeorm';
+import { Log } from 'src/home/entities/log.entity';
 
 @Injectable()
 export class TipoRecursoService {
-  create(createTipoRecursoDto: CreateTipoRecursoDto) {
-    return 'This action adds a new tipoRecurso';
+  constructor(
+    @InjectRepository(TipoRecurso)
+    private tipoRecursoRepository: Repository<TipoRecurso>,
+    @InjectRepository(Log)
+    private logRepository: Repository<Log>,
+  ) {}
+
+  async create(createTipoRecursoDto: CreateTipoRecursoDto) {
+    const newTipoRecurso =
+      this.tipoRecursoRepository.create(createTipoRecursoDto);
+
+    await this.tipoRecursoRepository.save(newTipoRecurso);
+
+    await this.logRepository.save({
+      tipo: 'Tipo Recurso',
+      mensaje: `Se ha creado un nuevo tipo de recurso: ${newTipoRecurso.nombre}`,
+    });
+
+    const tipoRecurso = await this.tipoRecursoRepository.find();
+
+    return {
+      message: 'Tipo de recurso creado exitosamente',
+      data: tipoRecurso,
+      status: 200,
+    };
   }
 
-  findAll() {
-    return `This action returns all tipoRecurso`;
+  async findAll() {
+    const tipoRecurso = await this.tipoRecursoRepository.find();
+
+    return {
+      message: 'Lista de tipos de recurso',
+      data: tipoRecurso,
+      status: 200,
+    };
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} tipoRecurso`;
+  async findOne(id: number) {
+    const tipoRecurso = await this.tipoRecursoRepository.findOneBy({
+      idTipoRecurso: id,
+    });
+
+    if (!tipoRecurso) {
+      throw new HttpException(
+        `Tipo de recurso con ID ${id} no encontrado`,
+        404,
+      );
+    }
+
+    return {
+      message: 'Tipo de recurso encontrado',
+      data: tipoRecurso,
+      status: 200,
+    };
   }
 
-  update(id: number, updateTipoRecursoDto: UpdateTipoRecursoDto) {
-    return `This action updates a #${id} tipoRecurso`;
+  async update(id: number, updateTipoRecursoDto: UpdateTipoRecursoDto) {
+    const tipoRecurso = await this.tipoRecursoRepository.findOneBy({
+      idTipoRecurso: id,
+    });
+
+    if (!tipoRecurso) {
+      throw new HttpException(
+        `Tipo de recurso con ID ${id} no encontrado`,
+        404,
+      );
+    }
+
+    await this.tipoRecursoRepository.update(id, updateTipoRecursoDto);
+
+    await this.logRepository.save({
+      tipo: 'Tipo Recurso',
+      mensaje: `Se ha actualizado el tipo de recurso ${tipoRecurso.nombre}`,
+    });
+
+    const tipoRecursos = await this.tipoRecursoRepository.find();
+
+    return {
+      message: 'Tipo de recurso actualizado exitosamente',
+      data: tipoRecursos,
+      status: 200,
+    };
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} tipoRecurso`;
+  async remove(id: number) {
+    const tipoRecurso = await this.tipoRecursoRepository.findOneBy({
+      idTipoRecurso: id,
+    });
+
+    if (!tipoRecurso) {
+      throw new HttpException(
+        `Tipo de recurso con ID ${id} no encontrado`,
+        404,
+      );
+    }
+
+    await this.tipoRecursoRepository.update(id, {
+      estado: false,
+    });
+
+    await this.logRepository.save({
+      tipo: 'Tipo Recurso',
+      mensaje: `Se ha eliminado el tipo de recurso ${tipoRecurso.nombre}`,
+    });
+
+    const tipoRecursos = await this.tipoRecursoRepository.find();
+
+    return {
+      message: 'Tipo de recurso eliminado exitosamente',
+      data: tipoRecursos,
+      status: 200,
+    };
   }
 }
