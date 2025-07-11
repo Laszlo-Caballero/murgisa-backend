3d9ee68b9c9af87a2d026eb8106fe7942809b440
AntonellaHonores <antonellahonores15@gmail.com>
2025-07-11 01:38:51 -0500

    preventivo

--- src/mantenimiento-preventivo/mantenimiento-preventivo.controller.ts
+++ src/mantenimiento-preventivo/mantenimiento-preventivo.controller.ts
@@ -1,8 +1,6 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { MantenimientoPreventivoService } from './mantenimiento-preventivo.service';
 import { CreateMantenimientoPreventivoDto } from './dto/create-mantenimiento-preventivo.dto';
-import { UpdateMantenimientoPreventivoDto } from './dto/update-mantenimiento-preventivo.dto';
-
 @Controller('mantenimiento-preventivo')
 export class MantenimientoPreventivoController {
   constructor(private readonly mantenimientoPreventivoService: MantenimientoPreventivoService) {}
@@ -21,12 +19,7 @@ export class MantenimientoPreventivoController {
   findOne(@Param('id') id: string) {
     return this.mantenimientoPreventivoService.findOne(+id);
   }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateMantenimientoPreventivoDto: UpdateMantenimientoPreventivoDto) {
-    return this.mantenimientoPreventivoService.update(+id, updateMantenimientoPreventivoDto);
-  }
-
+  
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.mantenimientoPreventivoService.remove(+id);
