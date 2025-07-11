a6273f32b8ab2ea939f0079188fcf2471478384f
luciana-31 <lgogny74@gmail.com>
2025-07-10 20:37:52 -0500

    feat(recurso): Agregar manejo de la entidad recurso y actualizar migraciones

--- src/recurso/recurso.module.ts
+++ src/recurso/recurso.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
 import { RecursoService } from './recurso.service';
 import { RecursoController } from './recurso.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Disponibilidad } from 'src/utils/entities/disponibilidad.entity';
+import { TipoRecurso } from 'src/tipo-recurso/entities/tipo-recurso.entity';
+import { Proveedor } from 'src/proveedor/entities/proveedor.entity';
+import { Recurso } from './entities/recurso.entity';
+
 
 @Module({
+    imports: [
+      TypeOrmModule.forFeature([Recurso, Disponibilidad, TipoRecurso, Proveedor]),
+    ],
   controllers: [RecursoController],
   providers: [RecursoService],
 })
