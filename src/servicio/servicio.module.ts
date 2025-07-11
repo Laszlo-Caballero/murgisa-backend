d007b5d6947a4b4be97d88c0df70f0c52d7de665
laszlo-caballero <laszlocaballero@gmail.com>
2025-07-11 01:09:38 -0500

    feat(servicio): agregar TipoServicio al módulo y crear migración para nuevas columnas en la tabla servicio

--- src/servicio/servicio.module.ts
+++ src/servicio/servicio.module.ts
@@ -4,9 +4,10 @@ import { Servicio } from './entities/servicio.entity';
 import { ServicioService } from './servicio.service';
 import { ServicioController } from './servicio.controller';
 import { Log } from 'src/home/entities/log.entity';
+import { TipoServicio } from 'src/tipo-servicio/entities/tipoServicio.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Servicio, Log])],
+  imports: [TypeOrmModule.forFeature([Servicio, Log, TipoServicio])],
   controllers: [ServicioController],
   providers: [ServicioService],
 })
