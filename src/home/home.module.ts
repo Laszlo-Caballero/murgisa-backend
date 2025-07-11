169af6ce3bfb7eab47d70e08e57763a06ea50c40
laszlo-caballero <laszlocaballero@gmail.com>
2025-07-11 15:30:45 -0500

    feat(home): agregar lógica para calcular totales y contar entidades en el servicio Home

--- src/home/home.module.ts
+++ src/home/home.module.ts
@@ -3,9 +3,21 @@ import { HomeService } from './home.service';
 import { HomeController } from './home.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Log } from './entities/log.entity';
+import { Venta } from 'src/venta/entities/venta.entity';
+import { Personal } from 'src/personal/entities/personal.entity';
+import { Servicio } from 'src/servicio/entities/servicio.entity';
+import { MantenimientoPreventivo } from 'src/mantenimiento-preventivo/entities/mantenimiento-preventivo.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Log])],
+  imports: [
+    TypeOrmModule.forFeature([
+      Log,
+      Venta,
+      MantenimientoPreventivo,
+      Personal,
+      Servicio,
+    ]),
+  ],
   controllers: [HomeController],
   providers: [HomeService],
 })
--- src/home/home.service.ts
+++ src/home/home.service.ts
@@ -1,11 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Log } from './entities/log.entity';
-import { Repository } from 'typeorm';
+import { Between, Repository } from 'typeorm';
+import { Venta } from 'src/venta/entities/venta.entity';
+import { MantenimientoCorrectivo } from 'src/mantenimiento-correctivo/entities/mantenimiento-correctivo.entity';
+import { Personal } from 'src/personal/entities/personal.entity';
+import { Servicio } from 'src/servicio/entities/servicio.entity';
+import { endOfMonth, startOfMonth } from 'date-fns';
+import { MantenimientoPreventivo } from 'src/mantenimiento-preventivo/entities/mantenimiento-preventivo.entity';
 
 @Injectable()
 export class HomeService {
-  constructor(@InjectRepository(Log) private logRepository: Repository<Log>) {}
+  constructor(
+    @InjectRepository(Log) private logRepository: Repository<Log>,
+    @InjectRepository(Venta) private ventaRepository: Repository<Venta>,
+    @InjectRepository(MantenimientoPreventivo)
+    private mantenimientoRepository: Repository<MantenimientoPreventivo>,
+    @InjectRepository(Personal)
+    private personalRepository: Repository<Personal>,
+    @InjectRepository(Servicio)
+    private servicioRepository: Repository<Servicio>,
+  ) {}
 
   async description() {
     const lastLog = await this.logRepository.find({
@@ -13,8 +28,78 @@ export class HomeService {
       take: 5,
     });
 
+    const ventas = await this.ventaRepository.find({
+      relations: [
+        'cliente',
+        'servicios',
+        'asignacionPersonal',
+        'asignacionPersonal.personal',
+        'asignacionPersonal.personal.profesion',
+        'detalleVenta',
+        'detalleVenta.recurso',
+        'detalleVenta.recurso.tipoRecurso',
+        'pagos',
+        'pagos.formaPago',
+      ],
+      order: { idVenta: 'DESC' },
+    });
+
+    const total = ventas
+      .filter((venta) => venta.estado)
+      .reduce((acc, venta) => {
+        const totalDetalle = venta.detalleVenta.reduce(
+          (sum, detalle) => sum + detalle.precio,
+          0,
+        );
+        const totalServicios = venta.servicios.reduce(
+          (sum, servicio) => sum + servicio.precio,
+          0,
+        );
+
+        const totalPersonal = venta.asignacionPersonal.reduce(
+          (sum, asignacion) => sum + asignacion.personal.sueldo,
+          0,
+        );
+        return acc + totalDetalle + totalServicios + totalPersonal;
+      }, 0);
+    const inicioMes = startOfMonth(new Date());
+    const finMes = endOfMonth(new Date());
+
+    const ordenes = await this.mantenimientoRepository.count({
+      where: {
+        estado: true,
+        fechaMantenimiento: Between(inicioMes, finMes),
+      },
+    });
+
+    const ordenesMatenimiento = await this.mantenimientoRepository.find({
+      where: { estado: true },
+      order: { mantenimientoPreventivoId: 'DESC' },
+      take: 3,
+    });
+
+    const personalCount = await this.personalRepository.count({
+      where: { estado: true },
+    });
+
+    const serviciosCount = await this.servicioRepository.count({
+      where: { estado: true },
+    });
+
     return {
-      actividades: lastLog,
+      message: 'Home data retrieved successfully',
+      status: 200,
+      data: {
+        actividades: lastLog,
+        //Cards
+        totalVentas: total,
+        mantenimiento: ordenes,
+        servicios: serviciosCount,
+        personal: personalCount,
+        //
+        lastVentas: ventas.slice(0, 3),
+        lastMantenimiento: ordenesMatenimiento,
+      },
     };
   }
 }
