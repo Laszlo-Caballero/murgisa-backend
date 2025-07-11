4f812fbba41dbe0ad169c371d02c7a44561f9ea8
laszlo-caballero <laszlocaballero@gmail.com>
2025-07-11 01:44:14 -0500

    feat(utils): agregar Horario al módulo de utilidades

--- src/utils/utils.module.ts
+++ src/utils/utils.module.ts
@@ -4,9 +4,10 @@ import { UtilsController } from './utils.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Ciudad } from 'src/cliente/entities/ciudades.entity';
 import { Disponibilidad } from './entities/disponibilidad.entity';
+import { Horario } from './entities/horario.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Ciudad, Disponibilidad])],
+  imports: [TypeOrmModule.forFeature([Ciudad, Disponibilidad, Horario])],
   controllers: [UtilsController],
   providers: [UtilsService],
 })
